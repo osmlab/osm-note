451aa9b3f803402a778b382247027288843f7a7a
Tom MacWright <tom@macwright.org>
2013-05-02 15:04:47 -0400

    Redesign

--- js/site.js
+++ js/site.js
@@ -1,49 +1,41 @@
-var map = L.map('map');
-map.attributionControl.setPrefix('');
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-var map_small = L.map('map-small', {
-    dragging: false,
-    attributionControl: false,
-    zoomControl: false
-});
-map.attributionControl.setPrefix('');
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_small);
-
-d3.select('.geolocate').on('click', function() {
-    d3.select(this).classed('pulse', true);
-    navigator.geolocation.getCurrentPosition(geolocated);
-});
-
-d3.select(window).on('load.addheight', addHeightClasses);
-d3.select('#map-small').on('click.back', unchosen);
-
-function addHeightClasses() {
+(function addHeightClasses() {
+    var TOP = 60; // top margin
     var style = document.createElement('style');
     style.type = 'text/css';
     var txt = '';
-    var fourth = window.innerHeight / 4;
+    var fourth = (window.innerHeight - TOP) / 4;
     for (var i = 1; i <= 4; i++) {
         txt += '.height' + i + '{height:' + (fourth * i) + 'px;}';
         txt += '.topmargin' + i + '{margin-top:' + (fourth * i) + 'px;}';
     }
     style.innerHTML = txt;
     document.getElementsByTagName('head')[0].appendChild(style);
-}
+})();
 
-d3.select('.accept-note').on('click', function() {
-    chosen(sel_marker.getLatLng());
-});
+var map = L.map('map', {
+    zoomControl: false,
+    attributionControl: false
+}).setView([0, 0], 2);
+
+L.tileLayer('http://a.tiles.mapbox.com/v3/tmcw.map-7s15q36b/{z}/{x}/{y}.png').addTo(map);
+
 var sel_marker = L.marker([0, 0], { draggable: true }).addTo(map);
 
-function geolocated(position) {
-    d3.select('.pane-1').style('display', 'none');
-    d3.select('.pane-2').style('display', 'block');
+$('.geolocate').on('click', function() {
+    navigator.geolocation.getCurrentPosition(geolocated);
+});
 
+$('.accept').on('click', function(e) {
+    e.preventDefault();
+    chosen(sel_marker.getLatLng());
+});
+
+$('.save').on('click', function(e) {
+    e.preventDefault();
+    save(sel_marker.getLatLng(), $('#note-comment').val());
+});
+
+function geolocated(position) {
     var meters = position.coords.accuracy;
 
     var dLat = meters / 111200,
@@ -55,43 +47,39 @@ function geolocated(position) {
 
     sel_marker.setLatLng([position.coords.latitude,
         position.coords.longitude]);
-    map.invalidateSize();
-}
 
-var marker_small = L.marker([0,0], { }).addTo(map_small);
+    map.fitBounds(bounds);
 
-d3.select('.save-note').on('click', function() {
-    save(marker_small.getLatLng(), d3.select('#note-comment').property('value'));
-});
-
-function unchosen() {
-    d3.select('.pane-2').style('display', 'block');
-    d3.select('.pane-3').style('display', 'none');
     map.invalidateSize();
 }
 
 function chosen(position) {
-    d3.select('.pane-2').style('display', 'none');
-    d3.select('.pane-3').style('display', 'block');
-
-    map_small.setView(position, 10);
-    marker_small.setLatLng(position);
-    map_small.invalidateSize();
-
-    d3.select('#note-comment').node().focus();
+    window.scrollTo(0, 502);
+    $('#note-comment').focus();
+    return false;
 }
+
+$('#note-comment').on('change', function() {
+    $('.pane-1')
+        .removeClass('to-accept')
+        .addClass('to-save');
+});
 
 function save(ll, comment) {
     var h = new window.XMLHttpRequest();
     h.open('POST', 'http://api.openstreetmap.org/api/0.6/notes.json', true);
-    h.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    h.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     h.send('lat=' + ll.lat + '&lon=' + ll.lng + '&text=' + encodeURIComponent(comment));
     h.onload = function(resp) {
         var j = JSON.parse(h.responseText);
-        d3.select('.pane-3').style('display', 'none');
-        d3.select('.pane-4').style('display', 'block');
-        d3.select('.osm-link')
-            .text('note #' + j.properties.id + ' on osm')
-            .attr('href', 'http://www.openstreetmap.org/?note=' + j.properties.id);
+        window.scrollTo(0, 0);
+        $('#note-comment').val('');
+        $('.pane-1').removeClass('to-save').addClass('to-accept');
+        $('.osm-link')
+            .attr('href', 'http://www.openstreetmap.org/?note=' + j.properties.id)
+            .removeClass('hide');
+        window.setTimeout(function() {
+            $('.osm-link').addClass('hide');
+        }, 5000);
     };
 }
